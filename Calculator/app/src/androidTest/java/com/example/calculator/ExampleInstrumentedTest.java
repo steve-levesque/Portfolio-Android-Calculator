af4a8f5b01ed6faf7655602f2b49d26a83679b31
Steve Lévesque <42849270+steve-levesque@users.noreply.github.com>
2020-12-16 11:14:14 -0500

    [Add] Unit Tests
    - Added new unit tests and javadoc format.

--- Calculator/app/src/androidTest/java/com/example/calculator/ExampleInstrumentedTest.java
+++ Calculator/app/src/androidTest/java/com/example/calculator/ExampleInstrumentedTest.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.calculator.utilities.DataUtility;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,10 +19,30 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    /**
+     * Desc: Context of the app under test.
+     * Class: None
+     * In: None
+     * Out: String ("com.example.calculator")
+     * Test: "com.example.calculator" == Out
+     */
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.calculator", appContext.getPackageName());
+    }
+
+    /**
+     * Desc: Check if on the physical test device, 1000 items are created by default.
+     * Class: {@link DataUtility}
+     * In: None
+     * Out: Int (1000)
+     * Test: 1000 == Out, 100 != Out
+     */
+    @Test
+    public void initialItemCount() {
+        // On the test device, 1000 items are created by default.
+        assertEquals(1000, DataUtility.count());
+        assertNotEquals(100, DataUtility.count());
     }
 }
--- Calculator/app/src/test/java/com/example/calculator/ExampleUnitTest.java
+++ Calculator/app/src/test/java/com/example/calculator/ExampleUnitTest.java
@@ -1,5 +1,10 @@
 package com.example.calculator;
 
+import android.content.Context;
+
+import com.example.calculator.models.CalculatorItem;
+import com.example.calculator.utilities.DataUtility;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +15,61 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    /**
+     * Desc:
+     * Class:
+     * In:
+     * Out:
+     * Test:
+     */
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    /**
+     * Desc: Mock test to show that InstrumentationRegistry is not available here.
+     * Class: None
+     * In: String
+     * Out: String
+     * Test: In == Out
+     */
+    @Test
+    public void useAppContext() {
+        // InstrumentationRegistry not available on host execution.
+        //Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.calculator", "com.example.calculator");
+    }
+
+    /**
+     * Desc: Check if item count is not differing from the intended count.
+     * Class: {@link DataUtility}
+     * In: None
+     * Out: Int (1000)
+     * Test: Out == 1000
+     */
+    @Test
+    public void initialItemCount() {
+        assertEquals(1000, DataUtility.count());
+    }
+
+    /**
+     * Desc: Tests the toString method of the Data class CalculatorItem.
+     * Class: {@link CalculatorItem#toString()}
+     * In: String ("content")
+     * Out: String ("content")
+     * Test: In == Out
+     */
+    @Test
+    public void CalculatorItem_toString() {
+        String content = "content";
+
+        CalculatorItem item = new CalculatorItem(
+                "1",
+                content,
+                "details"
+        );
+
+        assertEquals(item.toString(), content);
     }
 }
